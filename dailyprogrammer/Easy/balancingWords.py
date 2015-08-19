cdf8f711adaddaac0408d91f56b89e77d36f6f23
rwhitten <rrw3@att.net>
2015-08-18 22:00:58 -0600

    Added files to dailyprogrammer\easy

---
+++ dailyprogrammer/Easy/balancingWords.py
@@ -0,0 +1,41 @@
+from string import ascii_uppercase
+
+alphabet = dict.fromkeys(ascii_uppercase)
+
+# Assign values to letters
+counter = 1
+for c in ascii_uppercase:
+    alphabet[c] = counter
+    counter += 1
+
+def balance(entry):
+    # Weight on either side
+    left, right = 0, 0
+
+    for idx,val in enumerate(entry):
+        # Calculate leftweight
+        for subidx,subval in enumerate(reversed(entry[0:idx])):
+            left = (left + ((subidx + 1) * alphabet[subval]))
+
+        # Calculate rightweight
+        for subidx,subval in enumerate(entry[idx+1:]):
+            right = (right + (subidx +1) * alphabet[subval])
+
+        # Check if balanced
+        if left == right:
+            print(entry[:idx], val, entry[idx+1:], '-', left)
+            break
+        
+        # Check if impossible to balance
+        elif right < left:
+            print(entry, 'DOES NOT BALANCE')
+            break
+
+        # Reset weights
+        left, right = 0, 0
+
+
+balance('CONSUBSTANTIATION')
+balance('WRONGHEADED')
+balance('UNINTELLIGIBILITY')
+balance('SUPERGLUE')
---
+++ dailyprogrammer/Easy/wordsnake.py
@@ -0,0 +1,23 @@
+#words = "SHENANIGANS SALTY YOUNGSTER ROUND DOUBLET TERABYTE ESSENCE"
+#words = "CAN NINCOMPOOP PANTS SCRIMSHAW WASTELAND DIRK KOMBAT TEMP PLUNGE ESTER REGRET TOMBOY"
+    words = ("NICKEL LEDERHOSEN NARCOTRAFFICANTE EAT TO OATS SOUP PAST TELEMARKETER RUST THINGAMAJIG GROSS SALTPETER REISSUE ELEPHANTITIS").split()
+
+    #lay down base word
+    print(words[0])
+
+    #direction last word was written. 0 = right; 1 = down
+    direction = 0
+    #whitespace needed
+    padding = len(words[0]) - 1
+
+    for word in words[1:]:
+        #go down
+        if direction == 0:
+            for char in word[1:-1]:
+                print(' ' * padding + char)
+            direction = 1
+        #go right
+        elif direction == 1:
+            print(' ' * padding + word)
+            padding = padding + len(word) - 1
+            direction = 0;
